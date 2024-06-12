55c33490fbb9e60db93564f506b431306bf9b672
JavDog <122760247+JavDog@users.noreply.github.com>
2024-06-12 16:56:11 -0300

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,24 @@
+window.addEventListener('load', function() {
+    // Este código se ejecutará cuando la página se haya cargado completamente
+    if ("geolocation" in navigator) {
+        // El navegador soporta geolocalización
+        navigator.geolocation.getCurrentPosition(function(position) {
+            const div = document.getElementById('1');
+
+            // Cambia el contenido del div
+            div.innerText = 'ok';
+            var coords = "Latitud: " + position.coords.latitude + "<br>Longitud: " + position.coords.longitude;
+            // Puedes hacer lo que quieras con las coordenadas, como mostrarlas en un mensaje
+           // alert("Coordenadas GPS:\n" + coords);
+           
+
+            
+        }, function(error) {
+            // Manejar errores
+            console.error('Error obteniendo la ubicación:', error);
+        });
+    } else {
+        // El navegador no soporta geolocalización
+        alert("Lo siento, tu navegador no soporta geolocalización.");
+    }
+});
